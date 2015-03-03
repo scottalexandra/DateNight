b2573d7e9f5fd6aef5fee57191b405e11a25fc5f
Alex Robinson <alexandra.scott335@gmail.com>
2015-03-03 13:57:31 -0700

    added google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['google_oauth_key'], ENV['google_oauth_secret'], { :access_type => 'offline', :prompt => '' }
+  provider :google_oauth2, ENV['google_oauth_key'], ENV['google_oauth_secret']
 end
